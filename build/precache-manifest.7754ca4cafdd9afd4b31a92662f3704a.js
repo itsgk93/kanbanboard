5597e7c1a47a92826f34d0660853afb9a4d5b883
itsgk93 <itsgk93@gmail.com>
2021-02-19 01:49:22 +0530

    changes

---
+++ build/precache-manifest.7754ca4cafdd9afd4b31a92662f3704a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "947503cd88f81f00932e47e87d84dedf",
+    "url": "/kanbanboard/index.html"
+  },
+  {
+    "revision": "fa0c788b5e65ec32a905",
+    "url": "/kanbanboard/static/css/main.bc345dbb.chunk.css"
+  },
+  {
+    "revision": "960af09834ffc8319091",
+    "url": "/kanbanboard/static/js/2.1b369a02.chunk.js"
+  },
+  {
+    "revision": "fa0c788b5e65ec32a905",
+    "url": "/kanbanboard/static/js/main.506ccf6f.chunk.js"
+  },
+  {
+    "revision": "afc2dd2eaabf8c53dd7e",
+    "url": "/kanbanboard/static/js/runtime-main.371649b1.js"
+  }
+]);
